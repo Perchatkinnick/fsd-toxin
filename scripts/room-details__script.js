492b57655623a62907ceb24a2c7fef360b8f312d
Perchatkin Nick <koljaperchatkin@gmail.com>
2021-03-30 23:13:51 +1000

    finished room details

--- scripts/room-details__script.js
+++ scripts/room-details__script.js
@@ -1,6 +1,7 @@
 ﻿//@ts-check
 let roomsDataParth = [
     {
+
         //more info about room... and than ->
         reviews: [
             {
@@ -15,7 +16,9 @@ let roomsDataParth = [
                 text: 'Обслуживание на высоте! Всё аккуратно, чисто. Завтраки в номер советую заказать, каждый день новое блюдо и десерт как комплимент',
                 likes: 2,
             },
-        ]
+        ],
+        serviceFee: '-2179',
+        extraFee: '300',
     },
     {
         //next room
@@ -72,11 +75,86 @@ function onLoad() {
 
 function loadCalculator(roomData) {
     loadMainString(roomData);
+    loadFee();
+    loadExtraFee();
+    loadTotal();
+}
+
+function loadTotal() {
+    let parent = document.querySelector('.content__booking__calculator__total');
+    let priceElem = parent.children[2];
+
+    let roomPrice = +document.querySelector('.content__booking__calculator__main').children[2].innerHTML.split('\u20bd')[0];
+
+    let total = roomPrice + +roomsDataParth[0].serviceFee + +roomsDataParth[0].extraFee;
+
+    priceElem.innerHTML = total + '\u20bd';
+    loadSeparatorForTotal(parent.children[1]);
+}
+
+function loadSeparatorForTotal(parent) {
+    let separator = document.createElement('div');
+    separator.classList.add('content__booking__calculator__total__separator__body');
+
+    let previousSibling = parent.previousElementSibling;
+    let left = previousSibling.offsetWidth;
+    separator.style.left = left + 8 + 'px';
+
+    let containerWidth = parent.parentElement.offsetWidth;
+    separator.style.width = containerWidth - parent.parentElement.children[2].offsetWidth - left - 16 + 'px';
+
+    parent.appendChild(separator);
+}
+
+function loadExtraFee() {
+    let parent = document.querySelector('.content__booking__calculator__extrafee');
+    let valueElem = parent.children[0];
+    let priceElem = parent.children[2];
+
+    valueElem.innerHTML = 'Сбор за доп. услуги';
+    priceElem.innerHTML = roomsDataParth[0].extraFee + '\u20bd';
+    loadSeparator(parent.children[1]);
+}
+
+function loadFee() {
+    let parent = document.querySelector('.content__booking__calculator__fee');
+    let valueElem = parent.children[0];
+    let priceElem = parent.children[2];
+
+    let valueFee = +roomsDataParth[0].serviceFee;
+    let textValue = 'Сбор за услуги: ';
+    let textPrice = '\u20bd';
+    if (valueFee < 0) {
+        textValue += 'скидка ';
+        textPrice = '0' + textPrice;
+    } else {
+        textPrice = valueFee + textPrice;
+    }
+
+    textValue += Math.abs(valueFee) + '\u20bd';
+    valueElem.innerHTML = textValue;
+
+    priceElem.innerHTML = textPrice;
+
+    loadSeparator(parent.children[1]);
+}
+
+function loadSeparator(parent) {
+    let separator = document.createElement('div');
+    separator.classList.add('content__booking__calculator__separator__body');
+    separator.innerHTML = 'i'
+    let previousSibling = parent.previousElementSibling;
+    let left = previousSibling.offsetWidth;
+    separator.style.left = left + 8 + 'px';
+
+
+    parent.appendChild(separator);
 }
 
 function loadMainString(roomData) {
-    let valueElem = document.querySelector('.content__booking__calculator__main__value');
-    let priceElem = document.querySelector('.content__booking__calculator__main__price');
+    let parent = document.querySelector('.content__booking__calculator__main');
+    let valueElem = parent.children[0];
+    let priceElem = parent.children[2];
 
     let period = countDays();
     let coast = roomData.price;
